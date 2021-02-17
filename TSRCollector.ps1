44b0f36a89273cb2261b0c4f83389e7983ee76dc
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2021-02-16 20:35:44 -0600

    Create TSRCollector.ps1

---
+++ TSRCollector.ps1
@@ -0,0 +1,97 @@
+Function Invoke-TSRCollector {
+[CmdletBinding(
+    SupportsShouldProcess = $true,
+    ConfirmImpact = 'High')]
+    param($param)
+
+## Gather Tech Support Report Collector for all nodes in a cluster
+    CLS
+Function EndScript{  
+    break
+}
+$text=@"
+v1.0
+  _____ ___ ___    ___     _ _        _           
+ |_   _/ __| _ \  / __|___| | |___ __| |_ ___ _ _ 
+   | | \__ \   / | (__/ _ \ | / -_) _|  _/ _ \ '_|
+   |_| |___/_|_\  \___\___/_|_\___\__|\__\___/_|  
+                                                  
+by: Jim Gandy
+"@
+Write-Host $text
+$Title=@()
+    $Title+="Welcome to Tech Support Report Collector"
+    Write-host $Title
+    Write-host " "
+    Write-host "   This tool is used to collect TSRs from"
+    Write-host "   all nodes in a cluster"
+    Write-host " "
+    if ($PSCmdlet.ShouldProcess($param)) {
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+$user = "root"
+$pass= "calvin"
+$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
+$ShareIP=((Get-wmiObject Win32_networkAdapterConfiguration | ?{$_.IPEnabled}) | ?{$_.DefaultIPGateway.length -gt 0}).ipaddress[0]
+# Create a new folder and shares it
+    Write-Host "Creating a new shared folder to save the TSRs..."
+    $ShareName = "TSRdata"
+    $ShareFolder=$ENV:TEMP+"\"+$ShareName
+    New-Item -ItemType Directory -Force -Path $ShareFolder  >$null 2>&1
+    New-SmbShare -Name "TSRdata" -Path "$ShareFolder" -Temporary -FullAccess Everyone  >$null 2>&1
+    Write-Host "    Share location is $ShareFolder"
+# Gets the logged on creds
+    Write-Host "Gathering the credentials to access the share..."
+    $sus = $env:UserName
+    $sdom = (Get-WmiObject win32_computersystem).Domain
+    $ShareCreds=Get-Credential -Message "Enter credentials to access the share name to copy the TSR to the share." -UserName $sus
+# Gathers the iDRAC IP addresses from all nodes
+    Write-Host "Gathering the iDRAC IP Addresses from cluster nodes..."
+    $iDRACIPs=Invoke-Command -ComputerName (Get-ClusterNode -Cluster (Get-Cluster).Name) -ScriptBlock {
+    (Get-PcsvDevice).IPv4Address
+    }
+    Write-Host "    FOUND:$iDRACIPs"
+# Run TechSupportReport on each node
+    ForEach($IP in $iDRACIPs){
+        $result=@()
+        Write-Host "Collecting TSR from: $IP..."
+        $body = @{
+            "IPAddress"= $ShareIP;
+            "UserName"= $ShareCreds.UserName;
+            "Password"= $ShareCreds.GetNetworkCredential().Password;
+            "ShareName"= $ShareName;
+            "ShareType"="CIFS";
+            } | ConvertTo-Json
+        $idrac_ip=$IP
+        $URI="https://$idrac_ip/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportTechSupportReport"
+                           
+        Try{
+            $result=Invoke-WebRequest -UseBasicParsing -Uri $URI -Credential $credential -Method POST -Headers @{'content-type'='application/json';'Accept'='application/json'} -Body $body -ErrorVariable RespErr 
+            ($result.Content| ConvertFrom-Json).'@Message.ExtendedInfo'
+        }
+        Catch{
+            IF($RespErr){
+                $credential=Get-Credential
+                $result= Invoke-WebRequest -UseBasicParsing -Uri $URI -Credential $credential -Method POST -Headers @{'content-type'='application/json';'Accept'='application/json'} -Body $body -ErrorVariable RespErr
+            }
+        }
+    IF($result.StatusCode -eq 202){Write-Host "    StatusCode:"$result.StatusCode "Successfully scheduled TSR" -ForegroundColor Green }Else{Write-Host "    ERROR: StatusCode:" $result.StatusCode "Failed to scheduled TSR" -ForegroundColor Red}
+    }
+# Change directory to the shared folder were the TSRs will be put
+    cd $ShareFolder
+    Invoke-Expression "explorer ."
+    Write-Host "Please wait while TSRs are collected. Ussually this takes 2-5 minutes per node."
+    
+}
+}
