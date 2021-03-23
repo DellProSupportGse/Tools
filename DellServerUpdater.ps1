9a06d08bee9b252f94ba794901b5a8feb5911521
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2021-03-23 13:50:01 -0500

    Update DellServerUpdater.ps1
    Added Windows updates

--- DellServerUpdater.ps1
+++ DellServerUpdater.ps1
@@ -279,6 +279,11 @@ if ($PSCmdlet.ShouldProcess($param)) {
         If($IsClusterMember -eq "YES"){
             Run-ClusterPre
         }
+        # Check if Windows
+        IF([System.Environment]::OSVersion.VersionString -imatch 'Windows'){
+            Write-Host "Executing Windows Updates..."
+            Start-Process -FilePath "$env:comspec" -ArgumentList "/c del c:\ans.txt&&echo A>c:\ans.txt&&echo A>>c:\ans.txt&&cscript C:\Windows\System32\en-US\WUA_SearchDownloadInstall.vbs <c:\ans.txt&&del c:\ans.txt"
+        }        
         Write-Host "Executing DSU..."
         Run-DSU
     }
