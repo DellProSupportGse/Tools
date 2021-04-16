38c1675a9b36bd550dab5fe359d5e983fbfa6eec
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2021-04-16 16:50:44 -0500

    Update RunSDDC.ps1

--- RunSDDC.ps1
+++ RunSDDC.ps1
@@ -1,18 +1,11 @@
-    <#
-    .Synopsis
-       Invoke-SDDC
-    .DESCRIPTION
-       This script will remove old SDDC's, install new SDDC and Run SddcDiagnosticInfo
-    .EXAMPLE
-       Invoke-SDDC
-    #>
-    
-Function Invoke-SDDC {
+Function Invoke-SDDCOffline {
     [CmdletBinding(
         SupportsShouldProcess = $true,
         ConfirmImpact = 'High')]
         param($param)
     CLS
+$Run=Read-host "Ready to run? [y/n]"
+IF($Run -ine "y"){break}
 # Fix 8.3 temp paths
     $MyTemp=(Get-Item $env:temp).fullname
 # Clean old PrivateCloud.DiagnosticInfo
@@ -27,20 +20,37 @@ Function Invoke-SDDC {
                 Remove-Item "$p\PrivateCloud.DiagnosticInfo" -Force -Confirm:$False -Recurse -ErrorAction SilentlyContinue
             }
     }
+$SDDCFileCheck=Read-Host "Do you have the SDDC copied locally? [y/n]"
+IF($SDDCFileCheck -ine "y"){Write-Host "Please download from https://github.com/DellProSupportGse/PrivateCloud.DiagnosticInfo/archive/master.zip";break}
 
 # Fresh import of PrivateCloud.DiagnosticInfo
     # Allow Tls12 and Tls11 -- GitHub now requires Tls12
     # If this is not set, the Invoke-WebRequest fails with "The request was aborted: Could not create SSL/TLS secure channel."
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls11
     $module = 'PrivateCloud.DiagnosticInfo'; $branch = 'master'
-    Invoke-WebRequest -Uri https://github.com/DellProSupportGse/PrivateCloud.DiagnosticInfo/archive/master.zip -OutFile $MyTemp\$branch.zip
-    Expand-Archive -Path $MyTemp\$branch.zip -DestinationPath $MyTemp -Force
+    #Invoke-WebRequest -Uri https://github.com/DellProSupportGse/PrivateCloud.DiagnosticInfo/archive/master.zip -OutFile $MyTemp\$branch.zip
+    Function Get-CatFile($initialDirectory)
+            {
+                [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+                $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property @{Multiselect = $false}
+                $OpenFileDialog.Title = "Please Select local SDDC ZIP file..."
+                $OpenFileDialog.ShowHelp = 'Please download SDDDC from this link: https://github.com/DellProSupportGse/PrivateCloud.DiagnosticInfo/archive/master.zip'
+                $OpenFileDialog.initialDirectory = $initialDirectory
+                $OpenFileDialog.filter = "ZIP (*.zip)| *.zip"
+                $OpenFileDialog.ShowDialog() | Out-Null
+                $OpenFileDialog.filenames
+            }
+            $ProvidedSDDC=Get-CatFile("C:")
+            Write-host $ProvidedSDDC
+    Expand-Archive -Path $ProvidedSDDC -DestinationPath $MyTemp -Force
     $md = "$env:ProgramFiles\WindowsPowerShell\Modules"
     cp -Recurse $MyTemp\$module-$branch\$module $md -Force -ErrorAction Stop
-    rm -Recurse $MyTemp\$module-$branch,$MyTemp\$branch.zip
+    rm -Recurse $MyTemp\$module-$branch,$ProvidedSDDC
     $ModulePath=$md+"\"+$module
-    Import-Module $ModulePath -Force
+    Import-Module $ModulePath -Force -Verbose
+
     
 # Run SDDC
     Get-SddcDiagnosticInfo
 }
+Invoke-SDDCOffline 
