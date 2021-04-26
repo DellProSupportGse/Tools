d3bb3a7540d22c78d68120c8682097d78866a055
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2021-04-26 14:24:00 -0500

    Update DART.ps1

--- DART.ps1
+++ DART.ps1
@@ -250,8 +250,8 @@ if ($PSCmdlet.ShouldProcess($param)) {
         Set-Location 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
         $RegKeyPaths=Get-ChildItem | Select PSPath -ErrorAction SilentlyContinue 
         ForEach($Key in $RegKeyPaths){
-            $DSUVer=$_.DisplayVersion
             IF(Get-ItemProperty -Path $Key.PSPath | ?{$_.DisplayName -imatch 'DELL EMC System Update'}){
+                $DSUVer=Get-ItemProperty -Path $Key.PSPath.DisplayVersion
                 IF(Get-ItemProperty -Path $Key.PSPath | ?{[version]$_.DisplayVersion -ge [version]$LatestDSUVersion}){
                     Write-Host "    FOUND: DSU $DSUVer already installed" -ForegroundColor Green
                     $IsDSUInstalled="YES"
