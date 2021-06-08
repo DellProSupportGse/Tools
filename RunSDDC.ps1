6887091cdc8b778657b67fc156dd2c8c3604a06e
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2021-06-08 15:19:16 -0500

    Update RunSDDC.ps1
    added the ability to use the remote functionality of sddc

--- RunSDDC.ps1
+++ RunSDDC.ps1
@@ -58,7 +58,45 @@ Write-Host ""
     IF(Test-Path "$env:USERPROFILE\HealthTest-S2DCluster-*.zip"){Remove-Item $env:USERPROFILE\HealthTest-S2DCluster-*.zip -Force}    
 
 # Run SDDC
-    Get-SddcDiagnosticInfo
+    # Run SDDC if cluster service found on node
+    IF(Get-Service clussvc -ErrorAction SilentlyContinue){
+        Get-SddcDiagnosticInfo
+    }Else{
+        $ClusterToCollectLogsFrom=Read-Host "Please enter the name of the cluster to collect logs from"
+        # Check if we can connect to the cluster
+            Write-Host "Checking cluster connectivity..."
+            $i=0
+            While((Get-WmiObject -Class Win32_PingStatus -Filter "(Address='$ClusterToCollectLogsFrom') and timeout=1000").StatusCode -eq $null){
+                $i++
+                Write-Host "    WARNING: $ClusterToCollectLogsFrom not pingable. Please try again." -ForegroundColor Yellow
+                $ClusterToCollectLogsFrom=Read-Host "Please enter the name of the cluster to collect logs from"
+                IF($i -ge 3){
+                    Write-Host "ERROR: Too many attempts. Exiting..." -ForegroundColor Red
+                    break script
+                }
+            }
+            Write-Host "    SUCCESS: Able to ping $ClusterToCollectLogsFrom" -ForegroundColor Green
+                IF((Invoke-Command -ComputerName $ClusterToCollectLogsFrom -ErrorAction SilentlyContinue -ScriptBlock{(Get-cluster).name}) -imatch $ClusterToCollectLogsFrom){
+                    Write-Host "    SUCCESS: Able to connect to cluster" -ForegroundColor Green
+                    $CheckRSATClusteringPowerShell=IF((Get-WindowsFeature RSAT-Clustering-PowerShell).InstallState -eq 'Installed'){ 
+                        Write-Host "Execute: Get-SDDCDiagnosticInfo -ClusterName $ClusterToCollectLogsFrom..."
+                        Get-SDDCDiagnosticInfo -ClusterName $ClusterToCollectLogsFrom
+                    }Else{
+                        Write-Host "Remote SDDC requires RSAT-Clustering-PowerShell which requires a rebooted." -ForegroundColor Yellow
+                        IF((Read-Host "Would you like to install RSAT-Clustering-PowerShell [y/n]") -imatch 'y'){
+                            Install-WindowsFeature RSAT-Clustering-PowerShell
+                            IF((Get-WindowsFeature RSAT-Clustering-PowerShell).InstallState -eq 'InstallPending'){
+                                Write-Host "  SUCCESS: RSAT-Clustering-PowerShell installed, but a reboot is required to compelete the installation." -ForegroundColor Green
+                                IF((Read-Host "Reboot now [y/n]") -imatch 'y'){Restart-Computer -Force}Else{Write-Host "Please reboot to complete the installation" -ForegroundColor Yellow}
+                            }
+                        }Else{
+                            Write-Host "    Please Install-WindowsFeature RSAT-Clustering-PowerShell, reboot and rerun" -ForegroundColor Yellow}
+                        }
+                }Else{
+                    Write-Error "    ERROR: Failed to connect to cluster $ClusterToCollectLogsFrom. Please check the cluster name and run again."
+                }
+            }Else{Write-Error "ERROR: $ClusterToCollectLogsFrom is not pingable."}
+    }
 # Move to Logs if exists
 IF(Test-Path -Path "$MyTemp\logs"){
         Copy-Item -Path "$env:USERPROFILE\HealthTest-S2DCluster-*.zip" -Destination "$MyTemp\logs\"
