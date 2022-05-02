1c79c08fc64904fb25ee46d124f8d527c8f5a522
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2022-05-02 14:58:34 -0500

    1.3
    Change link to Azure blob

--- RunCluChk.ps1
+++ RunCluChk.ps1
@@ -1,10 +1,10 @@
 # Run CluChk
 # Created By: Jim Gandy
-# v1.2
+# v1.3
 Function Invoke-RunCluChk{
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "Downloading latest version..."
-$url = 'https://cluchk.gse.support/'
+$url = 'https://gsetools.blob.core.windows.net/cluchk/CluChk.ps1.remove?sv=2020-10-02&si=ReadAccess&sr=b&sig=BpFR0jPXaQUNNATR7%2FqNj7CXjNw9bJH8jmeX0melJxM%3D'
 $start_time = Get-Date
 Try{Invoke-WebRequest -Uri $url -OutFile $output -UseDefaultCredentials
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
@@ -20,4 +20,3 @@ Finally{
 #Variable Cleanup
 Remove-Variable * -ErrorAction SilentlyContinue
 }
-Invoke-RunCluChk
