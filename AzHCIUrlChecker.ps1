eab2f7cff7037a74486f74e48ed04918d6505d4b
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2022-05-13 00:52:27 -0500

    Update AzHCIUrlChecker.ps1

--- AzHCIUrlChecker.ps1
+++ AzHCIUrlChecker.ps1
@@ -57,7 +57,7 @@ Write-Host ""
             }
         }
     }Else{Write-Host "ERROR: Failed to get URL list from: $URL" -ForegroundColor Red }
-    $HCIURLs=@('[')+($URLs -replace 'http\:\/\/' -replace 'https\:\/\/' -replace '\/' -replace'\*\.' -replace '\`' -replace 'json' -replace '\[\{','{' -replace '\}\]','},' -replace '\}\s\]','}' -replace '\”','"' -replace '\“','"')+@(']') | Out-String | ConvertFrom-Json
+    $HCIURLs=@('[')+($URLs -replace 'http\:\/\/' -replace 'https\:\/\/' -replace '\/' -replace'\*\.' -replace '\`' -replace 'json' -replace '\[\{','{' -replace '\}\]','},' -replace '\}\s\]','}' -replace '”','"' -replace '“','"')+@(']') | Out-String | ConvertFrom-Json
     $URLs2Check=$HCIURLs  | sort URL -Unique
 
 # Check for running on cluster
