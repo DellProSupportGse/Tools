a1d5a7bcc77ea8b1b08d4b6c9dec6d3412926c27
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2022-05-13 00:24:11 -0500

    Update AzHCIUrlChecker.ps1

--- AzHCIUrlChecker.ps1
+++ AzHCIUrlChecker.ps1
@@ -18,8 +18,8 @@ v$Ver
    /_\   __| || |/ __|_ _| | | |_ _| |/ __| |_  ___ __| |_____ _ _ 
   / _ \ |_ / __ | (__ | || |_| | '_| | (__| ' \/ -_) _| / / -_) '_|
  /_/ \_\/__|_||_|\___|___|\___/|_| |_|\___|_||_\___\__|_\_\___|_|  
-                                                                   
-                                                      by: Jim Gandy
+                               
+                                                      by: Jim Gandy 
 "@
 Write-Host $text
 Write-Host ""
@@ -57,12 +57,15 @@ Write-Host ""
             }
         }
     }Else{Write-Host "ERROR: Failed to get URL list from: $URL" -ForegroundColor Red }
-    $HCIURLs=@('\[')+($URLs -replace 'http\:\/\/' -replace 'https\:\/\/' -replace '\/' -replace '\*\.' -replace '\`' -replace 'json' -replace '\[\{','{' -replace '\}\]','},' -replace '\}\s\]','}' -replace '\”','"' -replace '\“','"')+@('\]') | Out-String | Convertfrom-Json 
-    $URLs2Check=$HCIURLs  | Sort-Object URL -Unique
+    $HCIURLs=@('[')+($URLs -replace 'http\:\/\/' -replace 'https\:\/\/' -replace '\/' -replace '\*\.' -replace '`' -replace 'json' -replace '\[\{','{' -replace '\}\]','},' -replace '\}\s\]','}' -replace '\”','"' -replace '\“','"')+@(']') | Out-String | Convertfrom-Json 
+    $URLs2Check=$HCIURLs  | sort URL -Unique
 
 # Check for running on cluster
-IF(Get-Command Get-ClusterNode -ErrorAction SilentlyContinue){
-    $ServerList = (Get-ClusterNode -ErrorAction SilentlyContinue).Name
+IF(Get-Command Get-ClusterNode -ErrorAction SilentlyContinue -WarningAction SilentlyContinue ){
+    $ServerList = (Get-ClusterNode -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).Name
+}
+IF(-not($ServerList)){
+    $ServerList=$env:COMPUTERNAME
 }
 
 #Change buffer width to make reader frindly
@@ -77,7 +80,7 @@ IF(Get-Command Get-ClusterNode -ErrorAction SilentlyContinue){
 # Test connections
     foreach($Url in $URLs2Check) {
         Invoke-Command -ComputerName $ServerList -WarningAction SilentlyContinue -ScriptBlock {
-            $Result = Test-NetConnection -ComputerName ($Using:Url.URL) -Port ($Using:Url.Port) -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+            $Result = Test-NetConnection -ComputerName $($Using:Url.URL) -Port $($Using:Url.Port) -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
             If($Result.TcpTestSucceeded -eq $true) {Write-Host "PASSED: From $($env:COMPUTERNAME) to $($Using:Url.URL)" -ForegroundColor Green}
             If($Result.TcpTestSucceeded -eq $false) {Write-Host "FAILED: From $($env:COMPUTERNAME) to $($Using:Url.URL) INFO:$($Using:Url.Notes)" -ForegroundColor Red}
         }
