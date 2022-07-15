2ed498a814d5eef6cef2cdea448d7399e990ac67
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2022-07-15 14:59:59 -0500

    1.4

--- RunCluChkDev.ps1
+++ RunCluChkDev.ps1
@@ -1,10 +1,10 @@
 # Run CluChk Dev
 # Created By: Jim Gandy
-# v1.3
+# v1.4
 Function Invoke-RunCluChkDev{
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "Downloading latest version..."
-$url = 'https://gsetools.blob.core.windows.net/cluchk/CluChk_Dev.ps1.remove?sv=2020-10-02&st=2022-07-15T19%3A19%3A58Z&se=2025-07-16T19%3A19%3A00Z&sr=b&sp=r&sig=6iiXkmYccastGZKFJBMKSqqBHDZ%2BgkL8jeSmb%2FBIu6s%3D'
+$url = 'https://gsetools.blob.core.windows.net/cluchk/CluChkDEV.ps1.remove?sv=2020-10-02&st=2022-07-15T19%3A43%3A49Z&se=2025-07-16T19%3A43%3A00Z&sr=b&sp=r&sig=HjZEXdUzx8iT7P6BSZLsuIaRPX3ohBMjsAvH3jqXYmE%3D'
 $start_time = Get-Date
 Try{Invoke-WebRequest -Uri $url -UseDefaultCredentials
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
