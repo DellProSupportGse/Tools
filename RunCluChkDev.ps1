83afd735017ceaa9c38a80d70bc4028811f9fe03
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2023-06-05 13:22:54 -0500

    Update RunCluChkDev.ps1
    updated access policy

--- RunCluChkDev.ps1
+++ RunCluChkDev.ps1
@@ -5,6 +5,8 @@ Function Invoke-RunCluChkDev{
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "Downloading latest version..."
 $url = 'https://gsetools.blob.core.windows.net/cluchk/CluChkDEV.ps1.remove?SECRET REMOVED'
+#SECRET REMOVED'
+
 $start_time = Get-Date
 Try{Invoke-WebRequest -Uri $url -UseDefaultCredentials
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
