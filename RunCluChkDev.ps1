17a482375d8f9a2c1867ff72f57047b9158f7eb2
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2023-06-05 13:22:54 -0500

    Update RunCluChkDev.ps1
    updated access policy

--- RunCluChkDev.ps1
+++ RunCluChkDev.ps1
@@ -4,7 +4,9 @@
 Function Invoke-RunCluChkDev{
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "Downloading latest version..."
-$url = 'https://gsetools.blob.core.windows.net/cluchk/CluChkDEV.ps1.remove?sv=2020-10-02&st=2022-07-15T19%3A43%3A49Z&se=2025-07-16T19%3A43%3A00Z&sr=b&sp=r&sig=HjZEXdUzx8iT7P6BSZLsuIaRPX3ohBMjsAvH3jqXYmE%3D'
+$url = 'https://gsetools.blob.core.windows.net/cluchk/CluChkDEV.ps1.remove?sv=2021-10-04&si=ReadAccess&sr=c&sig=FPrKWcg0IRxzIDZHuu4syLjMSuLn%2Fco3gH1OLkYnPIs%3D'
+#sv=2020-10-02&st=2022-07-15T19%3A43%3A49Z&se=2025-07-16T19%3A43%3A00Z&sr=b&sp=r&sig=HjZEXdUzx8iT7P6BSZLsuIaRPX3ohBMjsAvH3jqXYmE%3D'
+
 $start_time = Get-Date
 Try{Invoke-WebRequest -Uri $url -UseDefaultCredentials
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
