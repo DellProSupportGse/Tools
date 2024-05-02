891f2461103e659a2e6679fdd4ad33178705b895
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-05-01 19:29:54 -0500

    Update Check-AzTenant.ps1

--- AzTools/Check-AzTenant.ps1
+++ AzTools/Check-AzTenant.ps1
@@ -3,7 +3,7 @@
 # This software contains the intellectual property of Dell Inc. or is licensed to Dell Inc. from third parties.
 # Use of this software and the intellectual property contained therein is expressly limited to the terms and
 # conditions of the License Agreement under which it is provided by or on behalf of Dell Inc. or its subsidiaries.
-Invoke-AzTenantCheck{
+
 param(
     [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
     [string]
@@ -14,6 +14,7 @@ param(
     ${subscriptionID}
 
 )
+Function Invoke-AzTenantCheck{
 function Get-AzureURIs {
     [CmdletBinding()]
     param (
