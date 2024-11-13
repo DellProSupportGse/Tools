505700d8b7b9a82e1e9cc5305b412c9f6b580c2e
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-11-13 13:43:28 -0600

    Update run_log_collect.sh

--- run_log_collect.sh
+++ run_log_collect.sh
@@ -1,2 +1,12 @@
-su
-curl -sSL https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/log_collect.sh -o ./log_collect.sh && chmod 755 log_collect.sh && bash ./log_collect.sh
+# Define the user you want to switch to
+TARGET_USER="root"
+
+# Define the script URL
+SCRIPT_URL="https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/log_collect.sh"
+
+# Run the commands as the target user
+sudo -u $TARGET_USER bash <<EOF
+curl -sSL $SCRIPT_URL -o ./log_collect.sh && \
+chmod 755 ./log_collect.sh && \
+bash ./log_collect.sh
+EOF
