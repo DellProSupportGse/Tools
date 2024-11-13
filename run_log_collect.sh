2e6c66c4d71b63c26451a3d8b3209ad4c84adebd
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-11-13 14:08:59 -0600

    Update run_log_collect.sh

--- run_log_collect.sh
+++ run_log_collect.sh
@@ -1,4 +1,9 @@
- if [ $(id -u) != 0 ]; then
-     echo "You're not root"
-     # elevate script privileges
- fi
+TARGET_USER="root"
+# Define the script URL
+SCRIPT_URL="https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/log_collect.sh"
+# Run the commands as the target user
+sudo -u $TARGET_USER bash <<EOF
+curl -sSL $SCRIPT_URL -o ./log_collect.sh && \
+chmod 755 ./log_collect.sh && \
+bash ./log_collect.sh
+EOF
