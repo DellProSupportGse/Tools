a722ce174c4e004de26f7eef010d0bc6f0901334
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-11-13 13:50:49 -0600

    Update run_log_collect.sh

--- run_log_collect.sh
+++ run_log_collect.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Switch to root user (ensure you have the necessary privileges to do so)
-if [ $(id -u) -ne 0 ]; then
-    exec sudo -i "$0" "$@"
-    exit $?
-fi
+# Prompt for the sudo password
+read -sp "Enter sudo password: " sudo_password
+echo
 
-# Commands to download and execute the script
-curl -sSL https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/run_log_collect.sh -o /root/run_log_collect.sh && chmod 755 /root/run_log_collect.sh && bash /root/run_log_collect.sh
+# SSH into the remote server and run the script with sudo
+ssh mystic@100.72.4.163 "echo '$sudo_password' | sudo -S bash -c 'curl -sSL https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/run_log_collect.sh -o /root/run_log_collect.sh && chmod 755 /root/run_log_collect.sh && bash /root/run_log_collect.sh'"
