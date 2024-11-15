f618a01de8af91b48ec21646e6aa6c8cb46c54c2
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2024-11-15 09:44:37 -0600

    v1.4

--- run_acp_log_collect.ps1
+++ run_acp_log_collect.ps1
@@ -9,7 +9,7 @@
 
 Function Invoke-RunAPEXlogsCollector {
 clear-host
-$ver="1.3"
+$ver="1.4"
 $titletext=@"
 $ver
     _   ___ _____  __  _                 ___     _ _        _           
@@ -18,19 +18,19 @@ $ver
  /_/ \_\_| |___/_/\_\ |____\___/\__, |  \___\___/_|_\___\__|\__\___/_|  
                                 |___/                                   
 "@
-Write-host ""
 Write-host $titletext
+Write-host ""
 
 # Ask for APEX VM IP
 [ipaddress]$axvmip=Read-host "Please enter the IP address of the APEX VM:"
 
 # Ask for Root password
-$rootpwd = Read-Host "Enter root password for APEX VM (input will be hidden):" -AsSecureString
+$rootpwd = Read-Host "Enter root password for APEX VM (input will be hidden and encrypted)" -AsSecureString
 
 # Download, run and remove log_collect.sh script
 Write-Host "Gathering logs..."
 Write-Host "Please enter the SSH password for APEX VM access:"
-$Result = ssh mystic@$($axvmip.IPAddressToString) "curl -sSL https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/log_collect.sh -o ./log_collect.sh && rm -f manual_log* && chmod 755 log_collect.sh && echo ""$([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($rootpwd)))"" | sudo -S bash ./log_collect.sh && rm ./log_collect.sh" *> $null 2>&1
+$Result = ssh mystic@$($axvmip.IPAddressToString) "curl -sSL https://raw.githubusercontent.com/DellProSupportGse/Tools/refs/heads/main/log_collect.sh -o ./log_collect.sh && rm -f manual_log* && chmod 755 log_collect.sh && echo ""$([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($rootpwd)))"" | sudo -S bash ./log_collect.sh && rm ./log_collect.sh"
 
 # Clear root password
 $rootpwd=""
