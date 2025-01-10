366f8e6744a57c9e530e5fbccd2487c6c5f9062e
Dell ProSupport GSE <79069476+DellProSupportGse@users.noreply.github.com>
2025-01-10 13:35:01 -0600

    v2.0 - Using driver file properties local and web for input file

--- FLCkr.ps1
+++ FLCkr.ps1
@@ -14,29 +14,26 @@ Function Invoke-FLCkr{
 param($param)
 Remove-Variable * -ErrorAction SilentlyContinue
 CLS
-$URL="https://raw.githubusercontent.com/MicrosoftDocs/windows-driver-docs/staging/windows-driver-docs-pr/ifs/allocated-altitudes.md"
 $text=@()
 Function EndScript{  
     break
 }
-$Ver="v1.0"
+$Ver="v2.0"
 $text = @"
 $Ver 
  ________  _____       ______  __               
 |_   __  ||_   _|    .' ___  |[  |  _           
   | |_ \_|  | |     / .'   \_| | | / ]  _ .--.  
-  |  _|     | |   _ | |        | '' <  [ `/'`\] 
- _| |_     _| |__/ |\ `.___.'\ | |`\ \  | |     
-|_____|   |________| `.____ .'[__|  \_][___]    
+  |  _|     | |   _ | |        | '' <  [ '/''\] 
+ _| |_     _| |__/ |\ '.___.'\ | |'\ \  | |     
+|_____|   |________| '.____ .'[__|  \_][___]    
                              by: Jim Gandy 
 "@
 Write-Host $text
     Write-host " "
     Write-host "Welcome to FLCkr"
     Write-host " "
-    Write-host "This tool lookups up filter drivers"
-    Write-host "in Microsoft's known good list"
-    Write-host "URL: $URL"
+    Write-host "    This tool lookups up filter drivers"
     Write-host " "
     $Run = Read-Host "Ready to run? [y/n]"
     Write-host " "
@@ -50,75 +47,130 @@ Function Get-FileName($initialDirectory)
     $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog -Property @{Multiselect = $true}
     $OpenFileDialog.Title = "Please Select FLTMC Output File"
     $OpenFileDialog.initialDirectory = $initialDirectory
-    #$OpenFileDialog.filter = "ZIP (*.zip)| *.zip"
     $OpenFileDialog.ShowDialog() | Out-Null
     $OpenFileDialog.filenames
 }
 
-Function Get-FLTMC($InputFile){
-    $parts=@()
-    $output=@()
-    If($InputFile -match "Run it locally"){$output = FLTMC}
-    Else{$output = Get-Content $InputFile}
-    If($output -match "Filter Name"){
-        $Found=($output|Select-String "Filter Name" -Context 0,1).LineNumber
-        $output=$output[($Found+1)..($output.length)]
+Function Get-FLTMCFromFile($InputFile){
+    Write-Host "    Reading from file..."
+    $filters=@()
+    $lines = Get-Content -Path $InputFile
+    foreach($line in $lines){
+        $line = $line.Trim() -split "\s{2,}"
+        if ($line.Length -ge 4 -and $line[0] -notmatch '^-+$' -and $line[0] -ne "Filter Name") {
+            $filters += [PSCustomObject]@{
+                FilterName    = $line[0]
+                NumInstances  = $line[1]
+                Altitude      = $line[2]
+                Frame         = $line[3]
+                WindowsDriver = $line[0] + ".sys"  # Assume the driver file matches FilterName
+            }
         }
-    $output | foreach {$parts = $_ -split "\s+", 6
-        New-Object -Type PSObject -Property @{
-                FilterName = ($parts[0])
-                NumInstances = $parts[1]
-                Altitude = $parts[2]
-                Frame = $parts[3]}
-        }
+    }
+    Parse-FiltersWeb $filters
 }
+
+Function Parse-FiltersWeb ($filters){
+    $URL="https://raw.githubusercontent.com/MicrosoftDocs/windows-driver-docs/staging/windows-driver-docs-pr/ifs/allocated-altitudes.md"
+    Write-Host "    Adding Company info from $URL..."
+    # use the credentials of the current user to authenticate on the proxy server
+    $Wcl = new-object System.Net.WebClient
+    $Wcl.Headers.Add("user-agent","PowerShell Script")
+    $Wcl.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials 
+    # gets the the list or known file system minifilter drivers
+    $KnownFltrs=Invoke-WebRequest -Uri $URL -UseBasicParsing | Select-Object RawContent
+    $RawKnownFltrs=$KnownFltrs.RawContent.split("`n") 
+    # Filter and parse lines
+    $KnownFltrs = $RawKnownFltrs | ForEach-Object {
+        # Trim whitespace and check for lines starting with "|"
+        if ($_ -match '^\|\s*(\S+)\s*\|\s*(\d+\.?\d*)\s*\|\s*(.+?)\s*\|$') {
+            [PSCustomObject]@{
+                Minifilter = $Matches[1]
+                Altitude   = $Matches[2]
+                Company    = $Matches[3]
+            }
+        }
+    }
+    # Find matches in $KnownFltrs
+    $matches = foreach ($filter in $filters) {
+        $match = $KnownFltrs | Where-Object {
+            $_.Minifilter -eq $filter.WindowsDriver -and $_.Altitude -eq $filter.Altitude
+        }
+
+        if ($match) {
+            # If match is found, construct the object
+            foreach ($m in $match) {
+                [PSCustomObject]@{
+                    FilterName    = $filter.FilterName
+                    NumInstances  = $filter.NumInstances
+                    Altitude      = $filter.Altitude
+                    Frame         = $filter.Frame
+                    WindowsDriver = $filter.WindowsDriver
+                    Company       = $m.Company
+                }
+            }
+        } else {
+            # If no match is found, add "UNKNOWN" as the Company
+            [PSCustomObject]@{
+                FilterName    = $filter.FilterName
+                NumInstances  = $filter.NumInstances
+                Altitude      = $filter.Altitude
+                Frame         = $filter.Frame
+                WindowsDriver = $filter.WindowsDriver
+                Company       = "NOT FOUND"
+            }
+        }
+    }
+    Write-Host "Filter Driver Lookup Results"
+    $matches | Format-Table
+}
+
+Function Get-FLTMCLocally {
+    # Run fltmc to get filter driver info
+    Write-Host "    Gathering FLTMC..."
+    $filters=@()
+    $filters = fltmc | ForEach-Object {
+        $line = $_.Trim() -split "\s{2,}"
+        if ($line.Length -ge 4 -and $line[0] -notmatch '^-+$' -and $line[0] -ne "Filter Name") {
+            [PSCustomObject]@{
+                FilterName    = $line[0]
+                NumInstances  = $line[1]
+                Altitude      = $line[2]
+                Frame         = $line[3]
+                WindowsDriver = $line[0] + ".sys"  # Assume the driver file matches FilterName
+            }
+        }
+    }
+     Parse-FiltersLocally $filters
+}
+
+Function Parse-FiltersLocally ($filters) {
+    Write-Host "    Adding Company/Description info from driver properties..."
+    # Add Company/Description information by checking driver properties
+	$filters | ForEach-Object {
+	    $driverPath = "C:\Windows\System32\drivers\$($_.WindowsDriver)"
+	    if (Test-Path $driverPath) {
+	        $_ | Add-Member -MemberType NoteProperty -Name Company -Value (Get-ItemProperty $driverPath).VersionInfo.CompanyName
+            $_ | Add-Member -MemberType NoteProperty -Name Description -Value (Get-ItemProperty $driverPath).VersionInfo.FileDescription
+	    } else {
+	        $_ | Add-Member -MemberType NoteProperty -Name Company -Value "Unable to find driver file locally"
+	    }
+	}
+    Write-Host "Filter Driver Lookup Results"
+    $filters | Format-Table
+}
+
 $Loc = Read-Host "Run FLTMC Locally? [y/n]"
 
     If($loc -ne ""){
         If($loc -ieq "n"){
             $InputFile=Get-FileName("C:")
             if(!$Input){EndScript}
-            $LocalFltmc=Get-FLTMC ($InputFile) | Select FilterName,NumInstances,Altitude,Frame
-        }Else{$LocalFltmc=Get-FLTMC("Run it locally") | Select FilterName,NumInstances,Altitude,Frame}
+            Get-FLTMCFromFile ($InputFile) 
+        }Else{Get-FLTMCLocally("Run it locally")}
     }Else{Endscript}
 
-#use the credentials of the current user to authenticate on the proxy server
-$Wcl = new-object System.Net.WebClient
-$Wcl.Headers.Add("user-agent","PowerShell Script")
-$Wcl.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials 
-#gets the the list or known file system minifilter drivers
-#$URL="https://raw.githubusercontent.com/MicrosoftDocs/windows-driver-docs/staging/windows-driver-docs-pr/ifs/allocated-altitudes.md"
-$KnownFltrs=Invoke-WebRequest -Uri $URL -UseBasicParsing | Select-Object RawContent
-$RawKnownFltrs=$KnownFltrs.RawContent.split("`n") 
-#Parse the output
-$FltCheck=@()
-$FltCheck="FilterName,NumInstances,Altitude,Frame,WindowsDriver,Company`r`n"
-ForEach($Driver in $LocalFltmc){
-    $Driver1=@()
-    $Driver1=$Driver.FilterName.Trim()
-    $FltCheck+=$Driver1+","+$Driver.NumInstances+","+$Driver.Altitude+","+$Driver.Frame+""
-    ForEach($Line in $RawKnownFltrs){
-        If($line -match $Driver1){
-            $S= $Line -split "\s+" , 6
-            $out=","+$S[1]+","+$S[5].Replace("|","")
-            $FltCheck+=$out
-        }
-    }
-    $FltCheck+=",N/A,N/A`r`n"
-}
-Write-Host "Filter Driver Lookup Results"
-$FltCheck|convertfrom-csv | FT -AutoSize
-<#$ScreenOut=$FltCheck|convertfrom-csv | FT -AutoSize
-$item=@()
-Write-PSObject -MatchMethod Exact -Column "Company" -Value "Microsoft" -ValueForeColor Yellow
-ForEach($item in $ScreenOut){
-    If($item -notmatch "Microsoft"){
-        Write-Host $item -ForegroundColor Yellow
-    }Else{Write-Host $item}
-}
-#>
-Write-Host "Driver lookup source URL: $URL"
-Write-Host ""
+
 $Run=Read-Host "Would you like to process another? [y/n]"
 If($Run -notmatch "y"){EndScript}
 }While($Run -eq "y")
