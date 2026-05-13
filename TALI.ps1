2f773f8d9bc3371ab8e1786358435440eec1ac44
TommyPaulkDell <107584414+TommyPaulkDell@users.noreply.github.com>
2026-05-13 15:54:08 -0500

    v0.44

--- TALI.ps1
+++ TALI.ps1
@@ -818,7 +818,7 @@ param(
         Foreach ($event in $events) {
             if ($event.Message -like "Checking version of PS module*") {
                 if ($event.Message -match "'([^']+)'.*?'([^']+)'.*?'([^']+)'.*?'([^']+)'") {
-                    if ((((Get-InstalledModule -Name $matches[1] -AllVersions).Version | %{[version]$_}) -gt [version]$matches[4]) -eq $true) {
+                    if ((((Get-InstalledModule -Name $matches[1] -AllVersions).Version | %{[version]$_}) -gt [version]$matches[4])) {
                         $badModules += [PSCustomObject]@{
                             ModuleName      = $matches[1]
                             NodeName        = $matches[2]
