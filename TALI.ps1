ed7154aebab0ad0662135317c54a77a56519b6ed
tadakk <107584414+TommyPaulkDell@users.noreply.github.com>
2026-05-17 23:48:00 -0500

    v0.465

--- TALI.ps1
+++ TALI.ps1
@@ -951,6 +951,7 @@ v$ver
         Write-Host "$prefix$Message" -ForegroundColor $Color
     }
 
+    <#
     # Unique report id
     $CReportID = [guid]::NewGuid().Guid
 
@@ -1005,7 +1006,7 @@ v$ver
         -TableName "TaliTelemetryData" `
         -PartitionKey $PartitionKey `
         -Data $data 
-
+#>
 
     If ($FixErrors -or $FixWarningsAlso) {Write-Warning "Fix commands are in beta and SHOULD NOT be used without proper guidance";sleep 5}
     If (($FixErrors -or $FixWarningsAlso) -and $ApproveAllFixesAutomatically) {Write-Warning "ApproveAllFixesAutomatically selected. All fixes will be applied!";sleep 10}
